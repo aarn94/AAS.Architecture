8c0c347b747fdc1089f999d7bbd16b4058566a66
Adrian Adamiak <a.adamiak@outlook.com>
2021-01-13 22:03:19 +0100

    Rename cors extensions

--- AAS.Architecture/Gateway/Cors/CorsExtensions.cs
+++ AAS.Architecture/Gateway/Cors/CorsExtensions.cs
@@ -7,28 +7,8 @@ namespace AAS.Architecture.Gateway.Cors
     public static class CorsExtensions
     {
         private const string WebStorageSectionName = "web";
-        
-        public static IConveyBuilder AddWebCors(this IConveyBuilder builder, string corsName = "AllowAll")
-        {
-            var webOptions = builder.GetOptions<WebOptions>(WebStorageSectionName);
-            
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy(corsName,
-                    builder =>
-                    {
-                        builder
-                            .WithOrigins(webOptions.Url)
-                            .AllowAnyMethod()
-                            .AllowAnyHeader()
-                            .AllowCredentials();
-                    });
-            });
-
-            return builder;
-        }
-        
-        public static IConveyBuilder AddCors(this IConveyBuilder builder, string corsName = "SecurityPolicy", string corsSectionName = "cors")
+                
+        public static IConveyBuilder AddSecuredCors(this IConveyBuilder builder, string corsName = "SecurityPolicy", string corsSectionName = "cors")
         {
             var webOptions = builder.GetOptions<CorsOptions>(corsSectionName);
             
@@ -50,6 +30,6 @@ namespace AAS.Architecture.Gateway.Cors
 
         public static IApplicationBuilder UseWebCors(this IApplicationBuilder builder, string corsName = "AllowAll") => builder.UseCors(corsName);
         
-        public static IApplicationBuilder UseCors(this IApplicationBuilder builder, string corsName = "SecurityPolicy") => builder.UseCors(corsName);
+        public static IApplicationBuilder UseSecuredCors(this IApplicationBuilder builder, string corsName = "SecurityPolicy") => builder.UseCors(corsName);
     }
 }
