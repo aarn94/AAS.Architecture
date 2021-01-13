77d2bab4e352b6ead0b8d0743ea9f3b109b345a5
Adrian Adamiak <a.adamiak@outlook.com>
2021-01-13 21:55:46 +0100

    fix

--- AAS.Architecture/Gateway/Cors/CorsExtensions.cs
+++ AAS.Architecture/Gateway/Cors/CorsExtensions.cs
@@ -7,26 +7,6 @@ namespace AAS.Architecture.Gateway.Cors
     public static class CorsExtensions
     {
         private const string WebStorageSectionName = "web";
-
-        public static IConveyBuilder SecureCors(this ConveyBuilder builder, string corsName = "SecurityPolicy", string corsSectionName = "cors")
-        {
-            var webOptions = builder.GetOptions<CorsOptions>(corsSectionName);
-            
-            ((IConveyBuilder) builder).Services.AddCors(options =>
-            {
-                options.AddPolicy(corsName,
-                    builder =>
-                    {
-                        builder
-                            .WithOrigins(webOptions.Domains)
-                            .WithMethods(webOptions.Methods)
-                            .WithExposedHeaders(webOptions.ExposedHeaders)
-                            .WithHeaders(webOptions.Headers);
-                    });
-            });
-
-            return builder;
-        }
         
         public static IConveyBuilder AddWebCors(this IConveyBuilder builder, string corsName = "AllowAll")
         {
@@ -47,7 +27,29 @@ namespace AAS.Architecture.Gateway.Cors
 
             return builder;
         }
+        
+        public static IConveyBuilder AddCors(this IConveyBuilder builder, string corsName = "SecurityPolicy", string corsSectionName = "cors")
+        {
+            var webOptions = builder.GetOptions<CorsOptions>(corsSectionName);
+            
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(corsName,
+                    builder =>
+                    {
+                        builder
+                            .WithOrigins(webOptions.Domains)
+                            .WithMethods(webOptions.Methods)
+                            .WithExposedHeaders(webOptions.ExposedHeaders)
+                            .WithHeaders(webOptions.Headers);
+                    });
+            });
+
+            return builder;
+        }
 
         public static IApplicationBuilder UseWebCors(this IApplicationBuilder builder, string corsName = "AllowAll") => builder.UseCors(corsName);
+        
+        public static IApplicationBuilder UseCors(this IApplicationBuilder builder, string corsName = "SecurityPolicy") => builder.UseCors(corsName);
     }
 }
